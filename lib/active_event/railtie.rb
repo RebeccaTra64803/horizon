92017d8bddabd6d14784904754de0e6dc177a6ab
Aaron Jensen <aaronjensen@gmail.com>
2013-05-28 23:28:07 -0700

    Comment out defunct railtie for now

--- lib/active_event/railtie.rb
+++ lib/active_event/railtie.rb
@@ -1,11 +1,11 @@
 module ActiveEvent
   class Railtie < Rails::Railtie
-    initializer "active_event.set_up_subscriber" do
-      suffix = /\.active_event\Z/
-      ActiveSupport::Notifications.subscribe suffix do |event, *args|
-        event_name = event.gsub(suffix, '').to_sym
-        SubscriptionContext.current.publish event_name, *args
-      end
-    end
+    # initializer "active_event.set_up_subscriber" do
+    #   suffix = /\.active_event\Z/
+    #   ActiveSupport::Notifications.subscribe suffix do |event, *args|
+    #     event_name = event.gsub(suffix, '').to_sym
+    #     SubscriptionContext.current.publish event_name, *args
+    #   end
+    # end
   end
 end
